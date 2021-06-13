4e6892a084bdb2e3a6c745bb55b5f042f7e90ffb
yash13902 <jasaniyash13@gmail.com>
2021-06-13 11:00:58 +0530

    Setup Login

--- app/src/main/java/com/example/hackmate/Fragments/GettingStartedFragment.java
+++ app/src/main/java/com/example/hackmate/Fragments/GettingStartedFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +15,23 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.hackmate.LoginActivity;
 import com.example.hackmate.MainActivity;
 import com.example.hackmate.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class GettingStartedFragment extends Fragment {
 
-
     View view;
+    Button create_profile;
+    LoginActivity loginActivity;
+    private EditText email_create_your_account;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,15 +44,15 @@ public class GettingStartedFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        LoginActivity loginActivity = (LoginActivity) getActivity();
+        initialise();
+        String email = getArguments().getString("email");
 
-        Button createProfile = view.findViewById(R.id.createProfile);
-        createProfile.setOnClickListener(new View.OnClickListener() {
+        create_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),MainActivity.class);
-                getActivity().startActivity(intent);
-                getActivity().finish();
+
+                checkIfEmailVerified(email);
+
             }
         });
 
@@ -62,8 +70,43 @@ public class GettingStartedFragment extends Fragment {
         gender_CompleteTextView.setText(gender_arrayAdapter.getItem(0).toString(), false);
         gender_CompleteTextView.setAdapter(gender_arrayAdapter);
 
+    }
 
+    public void initialise()
+    {
+        create_profile = view.findViewById(R.id.createProfile_button);
+        loginActivity = (LoginActivity) getActivity();
+//        email_create_your_account = view.findViewById(R.id.email_create_account);
+    }
 
+    public void checkIfEmailVerified(String email)
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        assert user != null;
+        if (user.isEmailVerified())
+        {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.putExtra("Email", email);
+            startActivity(intent);
+            loginActivity.finish();
+            Toast.makeText(getContext(), "Successfully logged in", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            // email is not verified, so just prompt the message to the user and restart this activity.
+            // NOTE: don't forget to log out the user.
+            Toast.makeText(getContext(), "Please Verify your Email to continue", Toast.LENGTH_SHORT).show();
+            FragmentManager fragmentManager = loginActivity.getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.bodyFragment, loginActivity.fragmentLogin)
+                    .addToBackStack(null)
+                    .commit();
+            FirebaseAuth.getInstance().signOut();
+
+            //restart this activity
+
+        }
     }
 
 }
--- app/src/main/java/com/example/hackmate/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/hackmate/Fragments/LoginFragment.java
@@ -9,20 +9,49 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hackmate.LoginActivity;
 import com.example.hackmate.MainActivity;
 import com.example.hackmate.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginFragment extends Fragment {
 
     View view;
+    private EditText email_login, password_login;
+    private TextView newAccount;
+    private LoginActivity loginActivity;
+    private FirebaseAuth mAuth;
+    private Button login;
+
+    @Override
+    public void onStart() {
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+
+            if (!mAuth.getCurrentUser().isEmailVerified()) {
+                Toast.makeText(getContext(), "Please Verify your Email to continue", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+
+            } else {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                loginActivity.finish();
+            }
+        }
+        super.onStart();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,9 +64,7 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView newAccount = view.findViewById(R.id.newAccount);
-
-        LoginActivity loginActivity = (LoginActivity) getActivity();
+        initialise();
 
         newAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,16 +79,90 @@ public class LoginFragment extends Fragment {
             }
         });
 
-        Button login = view.findViewById(R.id.button);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),MainActivity.class);
-                getActivity().startActivity(intent);
-                getActivity().finish();
+
+                String emailText = email_login.getText().toString();
+                String passWord = password_login.getText().toString();
+
+                if (email_login.getText().toString().isEmpty()) {
+                    email_login.setError("Email Required");
+                    email_login.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email_login.getText().toString()).matches()) {
+                    email_login.setError("Valid Email Required");
+                    email_login.requestFocus();
+                    return;
+                }
+                if (password_login.getText().toString().isEmpty()) {
+                    password_login.setError("Password Required");
+                    password_login.requestFocus();
+                    return;
+                }
+                if (password_login.getText().toString().length() < 6) {
+                    password_login.setError("Min 6 char required");
+                    password_login.requestFocus();
+                    return;
+                }
+
+                loginUser(emailText, passWord);
+
+
             }
         });
+
     }
+
+    public void initialise()
+    {
+        newAccount = view.findViewById(R.id.new_account);
+        email_login = view.findViewById(R.id.email_login);
+        password_login = view.findViewById(R.id.password_login);
+        mAuth = FirebaseAuth.getInstance();
+        loginActivity = (LoginActivity) getActivity();
+        login = view.findViewById(R.id.login_button);
+    }
+
+    public void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(getContext(), "Invalid Email / Password", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        checkIfEmailVerified(email);
+                    }
+                });
+    }
+
+    private void checkIfEmailVerified(String email)
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        assert user != null;
+        if (user.isEmailVerified())
+        {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.putExtra("Email", email);
+            startActivity(intent);
+            loginActivity.finish();
+            Toast.makeText(getContext(), "Successfully logged in", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            // email is not verified, so just prompt the message to the user and restart this activity.
+            // NOTE: don't forget to log out the user.
+            Toast.makeText(getContext(), "Please Verify your Email to continue", Toast.LENGTH_SHORT).show();
+            FirebaseAuth.getInstance().signOut();
+
+            //restart this activity
+
+        }
+    }
+
 
 }
