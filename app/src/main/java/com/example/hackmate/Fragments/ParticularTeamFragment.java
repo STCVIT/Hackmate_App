24782abb4dee9307be50f295407385dcba9e4618
Kavita1013 <78211425+Kavita1013@users.noreply.github.com>
2021-07-07 22:15:06 +0530

    Update ParticularTeamFragment.java

--- app/src/main/java/com/example/hackmate/Fragments/ParticularTeamFragment.java
+++ app/src/main/java/com/example/hackmate/Fragments/ParticularTeamFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,72 +17,59 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.hackmate.R;
+import com.example.hackmate.teamMemberAdapter;
+import com.example.hackmate.teamMember_Model;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ParticularTeamFragment extends Fragment {
+import java.util.ArrayList;
 
-    Button inviteTeammate, profileParticipant,addProjectTeam, editProject;
+public class ParticularTeamFragment extends Fragment {
+    private RecyclerView teamRV;
+    // Arraylist for storing data
+    private ArrayList<teamMember_Model> courseModelArrayList;
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_particular_team, container, false);
+        View v =inflater.inflate(R.layout.fragment_particular_team, container, false);
+
+//inflate appbar for this particular fragment
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.particularTeamAppBar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        return v;
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        inviteTeammate = view.findViewById(R.id.inviteTeammateButton);
-        profileParticipant = view.findViewById(R.id.profileParticipantButton);
-        addProjectTeam = view.findViewById(R.id.addProjectTeam);
-        editProject = view.findViewById(R.id.editProjectButton);
+        teamRV = view.findViewById(R.id.rvTeam);
 
-        inviteTeammate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment,new InviteParticipantFragment())
-                        .addToBackStack(null)
-                        .commit();
+// here we have created new array list and added data to it.
+        courseModelArrayList = new ArrayList<>();
 
-            }
-        });
+        courseModelArrayList.add(new teamMember_Model.TeamMemberModel("1.", "Deep Gandhi","itsverydeep@gmail.com","Leader",R.drawable.imageholder,true));
+        courseModelArrayList.add(new teamMember_Model.TeamMemberModel("2.", "Deep Gandhi","itsverydeep@gmail.com"," ",R.drawable.imageholder,true));
+        courseModelArrayList.add(new teamMember_Model.TeamMemberModel("3.", "Deep Gandhi","itsverydeep@gmail.com"," ",R.drawable.imageholder,true));
+        courseModelArrayList.add(new teamMember_Model.TeamMemberModel("4.", "Me","itsverydeep@gmail.com"," ",R.drawable.imageholder,false));
+        // we are initializing our adapter class and passing our arraylist to it.
+       teamMemberAdapter teamMemberAdapter = new teamMemberAdapter(getContext(), courseModelArrayList);
 
-        profileParticipant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment,new ParticipantProfileFragment())
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
 
-        addProjectTeam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment,new AddProjectFragment())
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        teamRV.setLayoutManager(linearLayoutManager);
+        teamRV.setAdapter(teamMemberAdapter);
 
-        editProject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment,new EditProjectFragment())
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
+
+
     }
 
     @Override
